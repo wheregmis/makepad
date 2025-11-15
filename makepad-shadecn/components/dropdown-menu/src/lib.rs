50855abd8035753076b6f59d3def7860add6b610
Sabin Regmi <get2sabin@gmail.com>
2025-11-15 18:20:46 -0500

    small refactor

--- makepad-shadecn/components/dropdown-menu/src/lib.rs
+++ makepad-shadecn/components/dropdown-menu/src/lib.rs
@@ -6,10 +6,144 @@ live_design! {
     use link::theme::*;
     use link::shaders::*;
 
+    // Custom PopupMenuItem for Shadecn design system
+    pub ShadecnPopupMenuItem = <PopupMenuItemBase> {
+        width: Fill,
+        height: Fit,
+        align: { y: 0.5 }
+        padding: {left: (SPACE_3), top: (SPACE_2), right: (SPACE_3), bottom: (SPACE_2)},
+
+        draw_text: {
+            instance active: 0.0
+            instance hover: 0.0
+            instance disabled: 0.0
+
+            uniform color: (COLOR_FG_PRIMARY),
+            uniform color_hover: (COLOR_FG_PRIMARY),
+            uniform color_active: (COLOR_FG_PRIMARY),
+            uniform color_disabled: (COLOR_FG_DISABLED),
+
+            text_style: <THEME_FONT_REGULAR> {
+                font_size: (FONT_BASE),
+            }
+
+            fn get_color(self) -> vec4 {
+                // Always return the primary color for visibility
+                return self.color
+            }
+        }
+
+        draw_bg: {
+            instance active: 0.0
+            instance hover: 0.0
+            instance disabled: 0.0
+
+            uniform gradient_border_horizontal: 0.0
+            uniform gradient_fill_horizontal: 0.0
+            uniform color_dither: 0.0
+            uniform border_size: 0.0
+            uniform border_radius: 0.0
+
+            uniform color: (COLOR_BG_PRIMARY),
+            uniform color_hover: (COLOR_BG_HOVER),
+            uniform color_active: (COLOR_BG_ACTIVE),
+            uniform color_disabled: (COLOR_BG_DISABLED),
+
+            uniform color_2: vec4(-1.0, -1.0, -1.0, -1.0)
+            uniform color_2_hover: vec4(-1.0, -1.0, -1.0, -1.0)
+            uniform color_2_active: vec4(-1.0, -1.0, -1.0, -1.0)
+            uniform color_2_disabled: vec4(-1.0, -1.0, -1.0, -1.0)
+
+            uniform border_color: vec4(0.0, 0.0, 0.0, 0.0)
+            uniform border_color_hover: vec4(0.0, 0.0, 0.0, 0.0)
+            uniform border_color_active: vec4(0.0, 0.0, 0.0, 0.0)
+            uniform border_color_disabled: vec4(0.0, 0.0, 0.0, 0.0)
+
+            uniform border_color_2: vec4(-1.0, -1.0, -1.0, -1.0)
+            uniform border_color_2_hover: vec4(-1.0, -1.0, -1.0, -1.0)
+            uniform border_color_2_active: vec4(-1.0, -1.0, -1.0, -1.0)
+            uniform border_color_2_disabled: vec4(-1.0, -1.0, -1.0, -1.0)
+
+            uniform mark_color: vec4(0.0, 0.0, 0.0, 0.0)
+            uniform mark_color_active: vec4(0.0, 0.0, 0.0, 0.0)
+            uniform mark_color_disabled: vec4(0.0, 0.0, 0.0, 0.0)
+
+            fn pixel(self) -> vec4 {
+                let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+                
+                let bg_color = mix(
+                    mix(
+                        mix(
+                            self.color,
+                            self.color_active,
+                            self.active
+                        ),
+                        self.color_hover,
+                        self.hover
+                    ),
+                    self.color_disabled,
+                    self.disabled
+                );
+
+                sdf.rect(
+                    0.0,
+                    0.0,
+                    self.rect_size.x,
+                    self.rect_size.y
+                );
+                sdf.fill(bg_color);
+
+                return sdf.result;
+            }
+        }
+    }
+
+    // Custom PopupMenuFlat for Shadecn design system
+    pub ShadecnPopupMenuFlat = <PopupMenuBase> {
+        width: Fill,
+        height: Fit,
+        flow: Down,
+        padding: {left: (SPACE_1), top: (SPACE_1), right: (SPACE_1), bottom: (SPACE_1)},
+
+        menu_item: <ShadecnPopupMenuItem> {}
+
+        draw_bg: {
+            uniform border_size: 1.0
+            uniform gradient_border_horizontal: 0.0
+            uniform gradient_fill_horizontal: 0.0
+            uniform border_radius: (RADIUS_MD)
+            uniform color: (COLOR_BG_PRIMARY)
+            uniform color_2: vec4(-1.0, -1.0, -1.0, -1.0)
+            uniform border_color: (COLOR_BORDER_PRIMARY)
+            uniform border_color_2: vec4(-1.0, -1.0, -1.0, -1.0)
+            uniform color_dither: 0.0
+
+            fn pixel(self) -> vec4 {
+                let sdf = Sdf2d::viewport(self.pos * self.rect_size);
+                
+                sdf.box(
+                    self.border_size,
+                    self.border_size,
+                    self.rect_size.x - self.border_size * 2.0,
+                    self.rect_size.y - self.border_size * 2.0,
+                    self.border_radius
+                );
+                sdf.fill_keep(self.color);
+
+                if self.border_size > 0.0 {
+                    sdf.stroke(self.border_color, self.border_size);
+                }
+
+                return sdf.result;
+            }
+        }
+    }
+
     pub ShadecnDropdownMenuBase = <DropDownFlat> {
         width: Fit,
         height: Fit,
         padding: {left: (SPACE_2), top: (SPACE_2), right: 28.0, bottom: (SPACE_2)},
+        popup_menu_position: BelowInput,
 
         draw_bg: {
             border_size: 1.0,
@@ -42,47 +176,7 @@ live_design! {
             color_disabled: (COLOR_FG_DISABLED),
         }
 
-        popup_menu: <PopupMenuFlat> {
-            draw_bg: {
-                border_size: 1.0,
-                border_radius: (RADIUS_MD),
-                gradient_border_horizontal: 0.0,
-                gradient_fill_horizontal: 0.0,
-                color_dither: 0.0,
-                color: (COLOR_BG_PRIMARY),
-                color_2: vec4(-1.0, -1.0, -1.0, -1.0),
-                border_color: (COLOR_BORDER_PRIMARY),
-                border_color_2: vec4(-1.0, -1.0, -1.0, -1.0),
-            }
-
-            menu_item = <PopupMenuItem> {
-                padding: {left: (SPACE_3), top: (SPACE_2), right: (SPACE_3), bottom: (SPACE_2)},
-
-                draw_bg: {
-                    gradient_border_horizontal: 0.0,
-                    gradient_fill_horizontal: 0.0,
-                    color_dither: 0.0,
-                    color: (COLOR_BG_PRIMARY),
-                    color_hover: (COLOR_BG_HOVER),
-                    color_active: (COLOR_BG_ACTIVE),
-                    color_disabled: (COLOR_BG_DISABLED),
-                    color_2: vec4(-1.0, -1.0, -1.0, -1.0),
-                    color_2_hover: vec4(-1.0, -1.0, -1.0, -1.0),
-                    color_2_active: vec4(-1.0, -1.0, -1.0, -1.0),
-                    color_2_disabled: vec4(-1.0, -1.0, -1.0, -1.0),
-                }
-
-                draw_text: {
-                    text_style: {
-                        font_size: (FONT_BASE),
-                    }
-                    color: (COLOR_FG_PRIMARY),
-                    color_hover: (COLOR_FG_PRIMARY),
-                    color_active: (COLOR_FG_PRIMARY),
-                    color_disabled: (COLOR_FG_DISABLED),
-                }
-            }
-        }
+        popup_menu: <ShadecnPopupMenuFlat> {}
     }
 
     pub ShadecnDropdownMenu = <ShadecnDropdownMenuBase> {}
--- makepad-shadecn/examples/shadecn-demo/src/components/scroll_area.rs
+++ makepad-shadecn/examples/shadecn-demo/src/components/scroll_area.rs
@@ -36,26 +36,26 @@ live_design! {
                         flow: Down,
                         spacing: (SPACE_2),
 
-                        <Label> { text: "Line 1" }
-                        <Label> { text: "Line 2" }
-                        <Label> { text: "Line 3" }
-                        <Label> { text: "Line 4" }
-                        <Label> { text: "Line 5" }
-                        <Label> { text: "Line 6" }
-                        <Label> { text: "Line 7" }
-                        <Label> { text: "Line 8" }
-                        <Label> { text: "Line 9" }
-                        <Label> { text: "Line 10" }
-                        <Label> { text: "Line 11" }
-                        <Label> { text: "Line 12" }
-                        <Label> { text: "Line 13" }
-                        <Label> { text: "Line 14" }
-                        <Label> { text: "Line 15" }
-                        <Label> { text: "Line 16" }
-                        <Label> { text: "Line 17" }
-                        <Label> { text: "Line 18" }
-                        <Label> { text: "Line 19" }
-                        <Label> { text: "Line 20" }
+                        <ShadecnLabel> { text: "Line 1" }
+                        <ShadecnLabel> { text: "Line 2" }
+                        <ShadecnLabel> { text: "Line 3" }
+                        <ShadecnLabel> { text: "Line 4" }
+                        <ShadecnLabel> { text: "Line 5" }
+                        <ShadecnLabel> { text: "Line 6" }
+                        <ShadecnLabel> { text: "Line 7" }
+                        <ShadecnLabel> { text: "Line 8" }
+                        <ShadecnLabel> { text: "Line 9" }
+                        <ShadecnLabel> { text: "Line 10" }
+                        <ShadecnLabel> { text: "Line 11" }
+                        <ShadecnLabel> { text: "Line 12" }
+                        <ShadecnLabel> { text: "Line 13" }
+                        <ShadecnLabel> { text: "Line 14" }
+                        <ShadecnLabel> { text: "Line 15" }
+                        <ShadecnLabel> { text: "Line 16" }
+                        <ShadecnLabel> { text: "Line 17" }
+                        <ShadecnLabel> { text: "Line 18" }
+                        <ShadecnLabel> { text: "Line 19" }
+                        <ShadecnLabel> { text: "Line 20" }
                     }
                 }
             }
